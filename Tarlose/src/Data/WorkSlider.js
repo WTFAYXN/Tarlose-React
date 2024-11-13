4d0ec1aedaf732fc3420b9a75189d53e8f1ace73
Ayan <132146120+WTFAYXN@users.noreply.github.com>
2024-11-13 10:40:25 +0000

    Work data added

---
+++ Tarlose/src/Data/WorkSlider.js
@@ -0,0 +1,57 @@
+import Cyberkalki from "../assets/Work/Cyberkalki.png";
+
+const WorkSlider =[
+ 
+    {
+        id:1,
+        class: "work-card-Cyberkalki",
+        Name:"Cyberkalki",
+        category:"Wordpress Website | 2024",
+        image:Cyberkalki,
+        url:"./work_live.html",
+        alt:"Cybersecurity Website",
+    },
+    {
+        id:2,
+        Name:"HealWithNehaSavara",
+        class: "work-card-HealwithNeha",
+        category:"Wix Website | 2024",
+        image:"../assets/Work/HealWithNeha.png",
+        url:"./work_live.html"
+    },
+    {
+        id:3,
+        Name:"Amorfume",
+        class: "works-card-Amorfume",
+        category:"MERN Website | 2024",
+        image:"../assets/Work/BettermindLabs.png",
+        url:"./work_live.html"
+    },
+    {
+        id:4,
+        class: "works-card-1",
+        Name:"BettermindLabs",
+        category:"Wix Website | 2024",
+        image:"../assets/Work/BettermindLabs.png",
+        url:"./work_live.html"
+    },
+    {
+        id:5,
+        Name:"BettermindLabs",
+        class: "works-card-1",
+        category:"Wix Website | 2024",
+        image:"../assets/Work/BettermindLabs.png",
+        url:"./work_live.html"
+    },
+    {
+        id:6,
+        Name:"BettermindLabs",
+        class: "works-card-1",
+        category:"Wix Website | 2024",
+        image:"../assets/Work/BettermindLabs.png",
+        url:"./work_live.html"
+    },
+    
+    
+];
+export default WorkSlider;
--- Tarlose/src/components/Work-slider/Work.jsx
+++ Tarlose/src/components/Work-slider/Work.jsx
@@ -2,194 +2,91 @@ import React, { useLayoutEffect } from "react";
 import gsap from "gsap";
 import { ScrollTrigger } from "gsap/ScrollTrigger";
 import "./Work.css"; // Ensure you have the necessary CSS
-import Cyberkalki from "../../assets/Work/Cyberkalki.png";
+import WorkSlider from "../../Data/WorkSlider";
+import { useNavigate } from "react-router-dom";
+// import image from "../../assets/Work/Cyberkalki.png";
+
 gsap.registerPlugin(ScrollTrigger);
 
 const Work = () => {
-    useLayoutEffect(() => {
-      let ctx = gsap.context(() => {
-        ScrollTrigger.matchMedia({
-          "(min-width: 800px)": function () {
-            const racesElement = document.querySelector(".races");
-            const racesWidth = racesElement.scrollWidth;
-  
-            // Horizontal scroll animation
-            gsap.to(".races", {
-              x: `-${racesWidth - window.innerWidth}px`,
-              scrollTrigger: {
-                trigger: ".racesWrapper",
-                start: "top 10%",
-                end: () => `+=${racesWidth * 0.8}px`, 
-                scrub: 2,
-                pin: true,
-                anticipatePin: 1,
-                invalidateOnRefresh: true,
-                // markers: true,
-              },
-            });
-          }
-        });
+  useLayoutEffect(() => {
+    let ctx = gsap.context(() => {
+      ScrollTrigger.matchMedia({
+        "(min-width: 800px)": function () {
+          const racesElement = document.querySelector(".races");
+          const racesWidth = racesElement.scrollWidth;
+
+          // Horizontal scroll animation
+          gsap.to(".races", {
+            x: `-${racesWidth - window.innerWidth}px`,
+            scrollTrigger: {
+              trigger: ".racesWrapper",
+              start: "top 10%",
+              end: () => `+=${racesWidth * 0.8}px`,
+              scrub: 2,
+              pin: true,
+              anticipatePin: 1,
+              invalidateOnRefresh: true,
+              // markers: true,
+            },
+          });
+        }
       });
-  
-      // Clean up ScrollTrigger instances on component unmount
-      return () => ctx.revert();
-    }, []);
-  
+    });
 
-    return(
-        <>
-        {/* <!-- gsap scroll-slider  -->
+    // Clean up ScrollTrigger instances on component unmount
+    return () => ctx.revert();
+  }, []);
 
-            <!-- work section  -->
- */}
+  const navigate = useNavigate();
 
+  return (
+    <>
+      <div className="racesWrapper text-white" id="Work">
+        <div className="races">
+          <div className="work-cont">
+            <h2 className="work-headding">
+              We let <span>our work</span> speak for itself.
+            </h2>
+            <p className="work-para">
+              Our experts develop customized native apps and software solutions
+              with innovative technologies for your success.
+            </p>
+            <button className="btn btn-contact px-5 py-2" type="button">
+              Contact Us
+            </button>
+          </div>
 
-<div className="racesWrapper text-white" id="Work" >
-    <div className="races" >
-        <div className="work-cont">
-            <h2 className="work-headding">We let <span>our work</span> speak for itself.</h2>
-            <p className="work-para">Our experts develop customized native apps and software solutions with
-                innovative
-                technologies for your success.</p>
-            <button className="btn btn-contact  px-5 py-2" type="button">Contact Us</button>
-        </div>
-
-        <div className="works-cards-cont my-5 d-grid g-5">
+          <div className="works-cards-cont my-5 d-grid g-5">
             <div className="row">
-                <div className="sub-card-cont d-flex">
-
-                    <a href="./work_live.html" className="card_link">
-                    <div className="works-card-2 ">
-                        <div className="cards-headding d-flex justify-content-between">
-                            <div className="title-work-card">
-                                <h3>Cyberkalki</h3>
-                                <h6 id="display-text">Wordpress Website | 2024</h6>
-                            </div>
-
-                            <p className="m-0">Case study</p>
+              <div className="sub-card-cont d-flex">
+                {WorkSlider.map((item) => (
+                  <a href={item.url} className="card_link" key={item.id}>
+                    <div className={item.class}>
+                      <div className="cards-headding d-flex justify-content-between">
+                        <div className="title-work-card">
+                          <h3>{item.Name}</h3>
+                          <h6 id="display-text">{item.category}</h6>
                         </div>
-
-                        <div className="work-img-cont">
-                            <img src={Cyberkalki} className="rounded img-fluid" alt=""/>
-                        </div>
-
+                        <p className="m-0">Case study</p>
+                      </div>
+                      <div className="work-img-cont">
+                        <img
+                          src={item.image}
+                          className="rounded img-fluid"
+                          alt={item.Name}
+                        />
+                      </div>
                     </div>
-                    </a>
-                    <a href="./work_live.html" className="card_link">
-                    <div className="works-card-1 ">
-                        <div className="cards-headding d-flex justify-content-between">
-                            <div className="title-work-card">
-                                <h3>My App</h3>
-                                <h6 id="display-text">App | 2024</h6>
-                            </div>
-
-                            <p className="m-0">Case study</p>
-                        </div>
-
-                        <div className="work-img-cont">
-                            <img src="assets/images/sample-card-img.avif" className="img-thumbnail" alt=""/>
-                        </div>
-
-                    </div>
-                    </a>
-                    <a href="./work_live.html" className="card_link">
-                    <div className="works-card-1 ">
-                        <div className="cards-headding d-flex justify-content-between">
-                            <div className="title-work-card">
-                                <h3>My App</h3>
-                                <h6 id="display-text">App | 2024</h6>
-                            </div>
-
-                            <p className="m-0">Case study</p>
-                        </div>
-
-                        <div className="work-img-cont">
-                            <img src="assets/images/sample-card-img.avif" className="img-thumbnail" alt=""/>
-                        </div>
-
-                    </div>
-                    </a>
-                    <a href="./work_live.html" className="card_link">
-                    <div className="works-card-1 ">
-                        <div className="cards-headding d-flex justify-content-between">
-                            <div className="title-work-card">
-                                <h3>My App</h3>
-                                <h6 id="display-text">App | 2024</h6>
-                            </div>
-
-                            <p className="m-0">Case study</p>
-                        </div>
-
-                        <div className="work-img-cont">
-                            <img src="assets/images/sample-card-img.avif" className="img-thumbnail" alt=""/>
-                        </div>
-
-                    </div>
-                    </a>
-                    <a href="./work_live.html" className="card_link">
-                    <div className="works-card-1 ">
-                        <div className="cards-headding d-flex justify-content-between">
-                            <div className="title-work-card">
-                                <h3>My App</h3>
-                                <h6 id="display-text">App | 2024</h6>
-                            </div>
-
-                            <p className="m-0">Case study</p>
-                        </div>
-
-                        <div className="work-img-cont">
-                            <img src="assets/images/sample-card-img.avif" className="img-thumbnail" alt=""/>
-                        </div>
-
-                    </div>
-                    </a>
-                    <a href="./work_live.html" className="card_link">
-                    <div className="works-card-1 ">
-                        <div className="cards-headding d-flex justify-content-between">
-                            <div className="title-work-card">
-                                <h3>My App</h3>
-                                <h6 id="display-text">App | 2024</h6>
-                            </div>
-
-                            <p className="m-0">Case study</p>
-                        </div>
-
-                        <div className="work-img-cont">
-                            <img src="assets/images/sample-card-img.avif" className="img-thumbnail" alt=""/>
-                        </div>
-
-                    </div>
-                    </a>
-                    <a href="./work_live.html" className="card_link">
-                    <div className="works-card-1 ">
-                        <div className="cards-headding d-flex justify-content-between">
-                            <div className="title-work-card">
-                                <h3>My App</h3>
-                                <h6 id="display-text">App | 2024</h6>
-                            </div>
-
-                            <p className="m-0">Case study</p>
-                        </div>
-
-                        <div className="work-img-cont">
-                            <img src="assets/images/sample-card-img.avif" className="img-thumbnail" alt=""/>
-                        </div>
-
-                    </div>
-                    </a>
-                    
-                </div>
-
+                  </a>
+                ))}
+              </div>
             </div>
-
-
+          </div>
         </div>
-
-
-    </div>
-</div>
-        </>
-    )
-}
+      </div>
+    </>
+  );
+};
 
 export default Work;
