3ff9021fce50a087581c1e049c0a4a6663d99bd8
jahangir2002 <jahangirshaikh553@gmail.com>
2025-06-29 13:15:29 +0530

    feat: Enhance BlogDetailPage layout and styling with new CSS variables and structure

--- client/src/components/Blog-tabs/BlogDetailPage.jsx
+++ client/src/components/Blog-tabs/BlogDetailPage.jsx
@@ -3,7 +3,7 @@ import Navbar from "../Navbar";
 import Footer from "../Footer";
 import Cursor from "../Cursor";
 import BlogTabsNav from "./BlogTabsNav";
-import "./BlogDetailPage.css"; 
+import "./BlogDetailPage.css";
 
 const BlogDetailPage = () => {
   return (
@@ -16,12 +16,14 @@ const BlogDetailPage = () => {
       <header className="blog-detail-header">
         <div className="container">
           <div className="grid">
-
             <div className="column-one">
               <h2>How AI Can Help You Deliver Multilingual Support</h2>
               <div className="PostAuthorsstyles">
                 <div className="AvatarsRow">
-                    <img src="https://hs-marketing-contentful.imgix.net/https%3A%2F%2Fimages.ctfassets.net%2Fp15sglj92v6o%2F1fdaLBuBELIc9AbULUzoDH%2Ffddea4af5e3d31715ddeeac25e3410f6%2FHow_AI_Can_Help_You_Scale_Multilingual_Support__1_.png?ixlib=gatsbySourceUrl-2.1.3&auto=format%2C%20compress&q=75&w=1678&h=942&s=96b2356ac8953ce9c6b50439f9e94ce7" alt="" />
+                  <img
+                    src="https://hs-marketing-contentful.imgix.net/https%3A%2F%2Fimages.ctfassets.net%2Fp15sglj92v6o%2F1fdaLBuBELIc9AbULUzoDH%2Ffddea4af5e3d31715ddeeac25e3410f6%2FHow_AI_Can_Help_You_Scale_Multilingual_Support__1_.png?ixlib=gatsbySourceUrl-2.1.3&auto=format%2C%20compress&q=75&w=1678&h=942&s=96b2356ac8953ce9c6b50439f9e94ce7"
+                    alt=""
+                  />
                   <a href="#">
                     <div className="Avatar size--S"></div>
                   </a>
@@ -37,16 +39,196 @@ const BlogDetailPage = () => {
             </div>
 
             <div className="column-two">
-                <div className="postImage"> 
-                    <img src="https://hs-marketing-contentful.imgix.net/https%3A%2F%2Fimages.ctfassets.net%2Fp15sglj92v6o%2F1fdaLBuBELIc9AbULUzoDH%2Ffddea4af5e3d31715ddeeac25e3410f6%2FHow_AI_Can_Help_You_Scale_Multilingual_Support__1_.png?ixlib=gatsbySourceUrl-2.1.3&auto=format%2C%20compress&q=75&w=1678&h=942&s=96b2356ac8953ce9c6b50439f9e94ce7" alt="" className="img-fluid" />
-                </div>
-
+              <div className="postImage">
+                <img
+                  src="https://hs-marketing-contentful.imgix.net/https%3A%2F%2Fimages.ctfassets.net%2Fp15sglj92v6o%2F1fdaLBuBELIc9AbULUzoDH%2Ffddea4af5e3d31715ddeeac25e3410f6%2FHow_AI_Can_Help_You_Scale_Multilingual_Support__1_.png?ixlib=gatsbySourceUrl-2.1.3&auto=format%2C%20compress&q=75&w=1678&h=942&s=96b2356ac8953ce9c6b50439f9e94ce7"
+                  alt=""
+                  className="img-fluid"
+                />
+              </div>
             </div>
-
-
           </div>
         </div>
       </header>
+
+      <div className="blog-detail-content">
+        <div className="blog-detail-flex">
+          <div className="blog-detail-text">
+            <p>
+              There’s an old saying that goes something like “The limits of my
+              language are the limits of my world.” It’s a beautiful sentiment and
+              a stark reminder that language, at its core, is really a tool by
+              which we’re able to express and explain things to others.
+            </p>
+            <p>
+              When our language is limited, asking or explaining gets incredibly
+              difficult. One year of high school Spanish might equip you to ask
+              someone where a bathroom is, but you probably won’t be able to have
+              deep philosophical conversations about the universe. You simply
+              don’t have the vocabulary.
+            </p>
+            <p>
+              The same can be true if you only offer support in one language.
+              People who don’t speak the language may find it difficult — or flat
+              out impossible — to get the help they need. As the world gets
+              smaller and customers are more likely to be from diverse
+              backgrounds, multilingual support options are becoming less a luxury
+              and more a necessity.
+            </p>
+            <p>
+              Previously, offering multilingual support was cost-prohibitive for a
+              lot of companies. They often had to hire staff in another country
+              (incurring recruiting costs), possibly set up a business entity in
+              order to hire locally, and then invest in software and technology to
+              keep in touch with those folks and make sure they were
+              well-supported. It was a lot.
+            </p>
+            <p>
+              However, with advancements in AI technology, multilingual support is
+              now a much more viable option, even for smaller teams. In this
+              article we cover why multilingual support is important, why you
+              should offer it, and ways AI technology is making it easier to do.
+            </p>
+
+            <h2>Why multilingual support is important</h2>
+            <p>
+              As customers have more options than ever, they also have a higher
+              level of agency when it comes to choosing who to purchase from.
+              Multilingual support is important because it shows a commitment to
+              your customers and that you value them and their experiences.
+            </p>
+            <p>
+              If there’s difficulty in the buying process, chances are people will
+              look elsewhere and seek out competitors who do cater to their needs.
+              In fact, research found 50% of shoppers will switch to a merchant
+              with less friction in the buying process.
+            </p>
+            <p>
+              That’s really what it comes down to: taking the time and making the
+              investment to improve the experience for current and potential
+              customers to the point that it’s a no-brainer for them to buy from
+              you.
+            </p>
+
+            <h2>How does AI multilingual support work?</h2>
+            <p>
+              Most AI tools used in customer support these days rely on three AI
+              technologies: natural language processing (NLP), generative AI, and
+              machine learning (ML).
+            </p>
+            <p>
+              NLP is the AI discipline that helps facilitate conversations between
+              humans and machines. It is able to understand not just the words a
+              person speaks or types, regardless of the language used, but also
+              understands the intent behind the words, and then respond to us (or
+              our customers) in kind.
+            </p>
+            <p>
+              Generative AI uses large language models (LLMs) to identify patterns
+              and structures within large sets of data to create original content.
+              Coupled with NLP, generative AI is the technology that powers
+              popular chatbots like ChatGPT. It can provide responses that flow
+              naturally and make more sense contextually than simple word for word
+              translations.
+            </p>
+            <p>
+              ML is the AI technology that facilitates learning with minimal human
+              intervention. In the example of a customer service chatbot, it is
+              the tech that observes whether the answers given by the bot are
+              right or wrong, and takes that information to improve accuracy and
+              fluency over time.
+            </p>
+
+            <h2>Language-specific vs. universal multi-language models</h2>
+            <p>
+              When it comes to supporting multiple languages, there are two
+              approaches that AI can take. It can either be trained on
+              language-specific models or on universal, multi-language models.
+            </p>
+            <p>
+              Most AI — including OpenAI's GPT-4, which is what is typically used
+              by support platforms — is trained on a universal, multi-language
+              model that doesn't rely on English or any specific language as
+              primary. Instead, it constructs its responses using learned
+              patterns, syntax, and cultural understanding to respond in whichever
+              language is required.
+            </p>
+            <p>
+              However, some argue that language-specific models are the best
+              approach as they allow for a lot more granularity when it comes to
+              cultural context. Most LLMs rely on training data that is mostly in
+              English, which means that they lack the context to account for local
+              language speakers. The models are often confused by things like
+              idioms or rely on phrases and words that native speakers wouldn't
+              use.
+            </p>
+            <p>
+              Since misunderstandings due to a lack of nuance are a risk of using
+              AI, businesses should approach the practice in multi-language
+              support settings with transparency.
+            </p>
+          </div>
+
+          <ul className="blog-detail-social SocialShare SocialShare--direction-VERTICAL">
+            <li>
+              <a href="">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  width="16"
+                  height="16"
+                  fill="currentColor"
+                  class="bi bi-twitter-x"
+                  viewBox="0 0 16 16"
+                >
+                  <path d="M12.6.75h2.454l-5.36 6.142L16 15.25h-4.937l-3.867-5.07-4.425 5.07H.316l5.733-6.57L0 .75h5.063l3.495 4.633L12.601.75Zm-.86 13.028h1.36L4.323 2.145H2.865z" />
+                </svg>
+              </a>
+            </li>
+            <li>
+              <a href="">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  width="16"
+                  height="16"
+                  fill="currentColor"
+                  class="bi bi-facebook"
+                  viewBox="0 0 16 16"
+                >
+                  <path d="M16 8.049c0-4.446-3.582-8.05-8-8.05C3.58 0-.002 3.603-.002 8.05c0 4.017 2.926 7.347 6.75 7.951v-5.625h-2.03V8.05H6.75V6.275c0-2.017 1.195-3.131 3.022-3.131.876 0 1.791.157 1.791.157v1.98h-1.009c-.993 0-1.303.621-1.303 1.258v1.51h2.218l-.354 2.326H9.25V16c3.824-.604 6.75-3.934 6.75-7.951" />
+                </svg>
+              </a>
+            </li>
+            <li>
+              <a href="">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  width="16"
+                  height="16"
+                  fill="currentColor"
+                  class="bi bi-linkedin"
+                  viewBox="0 0 16 16"
+                >
+                  <path d="M0 1.146C0 .513.526 0 1.175 0h13.65C15.474 0 16 .513 16 1.146v13.708c0 .633-.526 1.146-1.175 1.146H1.175C.526 16 0 15.487 0 14.854zm4.943 12.248V6.169H2.542v7.225zm-1.2-8.212c.837 0 1.358-.554 1.358-1.248-.015-.709-.52-1.248-1.342-1.248S2.4 3.226 2.4 3.934c0 .694.521 1.248 1.327 1.248zm4.908 8.212V9.359c0-.216.016-.432.08-.586.173-.431.568-.878 1.232-.878.869 0 1.216.662 1.216 1.634v3.865h2.401V9.25c0-2.22-1.184-3.252-2.764-3.252-1.274 0-1.845.7-2.165 1.193v.025h-.016l.016-.025V6.169h-2.4c.03.678 0 7.225 0 7.225z" />
+                </svg>
+              </a>
+            </li>
+            <li>
+              <a href="">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  width="16"
+                  height="16"
+                  fill="currentColor"
+                  class="bi bi-envelope"
+                  viewBox="0 0 16 16"
+                >
+                  <path d="M0 4a2 2 0 0 1 2-2h12a2 2 0 0 1 2 2v8a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2zm2-1a1 1 0 0 0-1 1v.217l7 4.2 7-4.2V4a1 1 0 0 0-1-1zm13 2.383-4.708 2.825L15 11.105zm-.034 6.876-5.64-3.471L8 9.583l-1.326-.795-5.64 3.47A1 1 0 0 0 2 13h12a1 1 0 0 0 .966-.741M1 11.105l4.708-2.897L1 5.383z" />
+                </svg>
+              </a>
+            </li>
+          </ul>
+        </div>
+      </div>
 
       <Footer />
     </>
