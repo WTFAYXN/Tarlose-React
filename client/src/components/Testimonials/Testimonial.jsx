bf357b33ca67ab5570b3030353076f2f02268b4f
jahangir2002 <jahangirshaikh553@gmail.com>
2025-07-12 15:44:16 +0530

    feat: Add right section to Testimonial component with new card layout and styling improvements

--- client/src/components/Testimonials/Testimonial.jsx
+++ client/src/components/Testimonials/Testimonial.jsx
@@ -41,16 +41,55 @@ const Testimonial = () => {
               </div>
               <hr className="Testimonial-divider" />
               <div className="button-group Testimonial-btn">
-          <button className="btn shining-button"><a href="https://calendly.com/ayansayad2005/30min?month=2024-12">
-              Book a Free Call
-            </a></button>
-          <button className="btn shining-button">See Projects</button>
-        </div>
+                <button className="btn shining-button">
+                  <a href="https://calendly.com/ayansayad2005/30min?month=2024-12">
+                    Book a Free Call
+                  </a>
+                </button>
+                <button className="btn shining-button">See Projects</button>
+              </div>
             </div>
-
-
           </div>
-          <div className="Testimonial-right-container"></div>
+          <div className="Testimonial-right-container">
+            <div className="Testimonial-cards">
+              <div className="Testimonial-card">
+                <div className="Testimonial-card-heading-container">
+                  <div className="Testimonial-img-container">
+                    <img src="https://framerusercontent.com/images/eFraHAe16muHsBuOQT8J7PtnM.png" alt="" className="Testimonial-img" />
+                  </div>
+                  <div className="Testimonial-card-heading">
+                    <h3>Will smith</h3>
+                    <p>Harper education</p>
+                  </div>
+                </div>
+
+                <hr className="Testimonial-card-divider" />
+
+                <div className="Testimonial-card-content">
+                  <div className="Testimonial-rating">
+                    <p className="m-0">5.0</p>
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      viewBox="0 0 24 24"
+                      fill="var(--token-c858731e-e94e-456c-870f-c18178498e78, rgb(255, 215, 0))"
+                      width="18px"
+                      height="18px"
+                    >
+                      <path d="M12 17.27L18.18 21l-1.64-7.03L22 9.24l-7.19-.61L12 2 9.19 8.63 2 9.24l5.46 4.73L5.82 21z"></path>
+                    </svg>
+                  </div>
+
+                  <div className="Testimonial-review">
+                    <p>
+                      The designs exceeded our expectations! Every element felt
+                      purposeful, creating a seamless and visually stunning
+                      brand identity
+                    </p>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
         </div>
       </div>
     </>
