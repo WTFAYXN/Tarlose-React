ebe58d4bae4f648c7d0b3c8e16bffba60c660a86
jahangir2002 <jahangirshaikh553@gmail.com>
2025-07-17 11:54:18 +0530

    feat: Update Portfolio component styles and structure for improved layout and readability

--- client/src/pages/Portfolio/Portfolio.jsx
+++ client/src/pages/Portfolio/Portfolio.jsx
@@ -8,20 +8,24 @@ const Portfolio = () => {
         <section className="portfolio-section container">
           <div className="portfolio-container">
             <div className="portfolio-left-container">
-              <h2>Fade</h2>
-              <p>
+              <h2 className="m-0">Fade</h2>
+              <p className="m-0">
                 Fade is a premium portfolio template designed to showcase work
                 with elegance and precision, perfect for designers, developers,
                 and creative professionals.
               </p>
-              <button>Live Site Preview</button>
+              <button className="btn shining-button">
+                    <a href="https://calendly.com/ayansayad2005/30min?month=2024-12">
+                      Live Site Preview
+                    </a>
+                  </button>
               <div className="project-container">
                 <div className="client">
-                  <p>Client</p>
+                  <p className="m-0">Client</p>
                   <h6>Framer Template</h6>
                 </div>
                 <div className="Service-provider">
-                  <p>Service Provided</p>
+                  <p className="m-0">Service Provided</p>
                   <h6>Web Design, Web Development</h6>
                 </div>
               </div>
