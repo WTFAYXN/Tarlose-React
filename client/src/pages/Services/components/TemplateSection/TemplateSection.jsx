4bc83f9eefdc83f5fdfd218ac759d5ea3d6c804a
jahangir2002 <jahangirshaikh553@gmail.com>
2025-07-22 10:18:20 +0530

    style: Enhance TemplateSection component with updated layout, including a new header and description for improved user engagement

--- client/src/pages/Services/components/TemplateSection/TemplateSection.jsx
+++ client/src/pages/Services/components/TemplateSection/TemplateSection.jsx
@@ -1,11 +1,16 @@
-import React from 'react';
+import React from "react";
+import "./TemplateSection.css"
 
 const TemplateSection = () => {
   return (
     <>
-    <h2>Top-Performing Templates for Every Need</h2>
+      <div className="Tempalte-parent-container container">
+        <div className="Tempalte-header d-flex flex-column text-center">
+          <h2>Top-Performing Templates for Every Need</h2>
+          <p>Explore versatile templates crafted for any purpose.</p>
+        </div>
+      </div>
     </>
-   
   );
 };
 
