9ba3b93648125d75277e50da4b9242c4c490d007
jahangir2002 <jahangirshaikh553@gmail.com>
2025-07-26 13:06:17 +0530

    style: Update HowItWorks component with new parent container, enhanced layout, and responsive design adjustments for improved user experience

--- client/src/pages/Services/components/HowItWorks/HowItWorks.jsx
+++ client/src/pages/Services/components/HowItWorks/HowItWorks.jsx
@@ -3,54 +3,66 @@ import "./HowItWorks.css";
 
 const HowItWorks = () => {
   return (
-    <div className="how-it-works-container container">
-      <h2 className="how-it-works-title">How It Works?</h2>
-      <div className="steps-container">
-        <div className="step">
-          <div className="dot-line-container">
-            <div className="step-dot"></div>
-            <div className="work-step-line-container">
-                 <hr className="work-step-line m-0" />
-            </div>
-          </div>
 
-          <div className="step-content">
-            <h3>Explore Notionary</h3>
-            <p>Visit our website anytime from your phone or desktop.</p>
+    <>
+    <div className="how-it-works-parent-container container">
+ <div className="how-it-works-container ">
+      <div className="how-it-works-heading">
+        <h2 className="how-it-works-title">How It Works?</h2>
+      </div>
+
+      <div className="steps-parent-container">
+        <div className="steps-container">
+          <div className="step">
+            <div className="dot-line-container">
+              <div className="step-dot"></div>
+              <div className="work-step-line-container">
+                <hr className="work-step-line m-0" />
+              </div>
+            </div>
+
+            <div className="step-content">
+              <h3>Explore Notionary</h3>
+              <p>Visit our website anytime from your phone or desktop.</p>
+            </div>
           </div>
         </div>
-      </div>
-      <div className="steps-container">
-        <div className="step">
-          <div className="dot-line-container">
-            <div className="step-dot"></div>
-            <div className="work-step-line-container">
-                 <hr className="work-step-line m-0" />
+        <div className="steps-container">
+          <div className="step">
+            <div className="dot-line-container">
+              <div className="step-dot"></div>
+              <div className="work-step-line-container">
+                <hr className="work-step-line m-0" />
+              </div>
             </div>
-          </div>
 
-          <div className="step-content">
-            <h3>Explore Notionary</h3>
-            <p>Visit our website anytime from your phone or desktop.</p>
+            <div className="step-content">
+              <h3>Explore Notionary</h3>
+              <p>Visit our website anytime from your phone or desktop.</p>
+            </div>
           </div>
         </div>
-      </div>
-      <div className="steps-container">
-        <div className="step">
-          <div className="dot-line-container">
-            <div className="step-dot"></div>
-            <div className="work-step-line-container">
-                 <hr className="work-step-line m-0" />
+        <div className="steps-container">
+          <div className="step">
+            <div className="dot-line-container">
+              <div className="step-dot"></div>
+              <div className="work-step-line-container">
+                <hr className="work-step-line m-0" />
+              </div>
             </div>
-          </div>
 
-          <div className="step-content">
-            <h3>Explore Notionary</h3>
-            <p>Visit our website anytime from your phone or desktop.</p>
+            <div className="step-content">
+              <h3>Explore Notionary</h3>
+              <p>Visit our website anytime from your phone or desktop.</p>
+            </div>
           </div>
         </div>
       </div>
     </div>
+    </div>
+    </>
+
+   
   );
 };
 
