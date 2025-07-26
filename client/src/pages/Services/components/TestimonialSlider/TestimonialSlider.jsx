917f7bd9c36741a139f5dad98f89aa955f00a983
jahangir2002 <jahangirshaikh553@gmail.com>
2025-07-26 11:54:45 +0530

    style: Adjust TestimonialSlider CSS for improved height management and refine animation transitions; add multiple testimonials for enhanced user engagement

--- client/src/pages/Services/components/TestimonialSlider/TestimonialSlider.jsx
+++ client/src/pages/Services/components/TestimonialSlider/TestimonialSlider.jsx
@@ -139,6 +139,140 @@ const TestimonialSlider = () => {
 
           </div>
         </div>
+        <div className="vertical-slide">
+          <div className="vertical-slide-heading-container">
+            <div className="vertical-slide-img">
+              <img
+                src="https://framerusercontent.com/images/lN1CfuAulx1IJ6VQhc4Pbe1sXSA.png"
+                alt=""
+              />
+            </div>
+
+            <div className="slider-heading">
+              <h5>Jack</h5>
+              <div class="Testimonial-rating">
+                <p class="m-0">5</p>
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  viewBox="0 0 24 24"
+                  fill="var(--token-c858731e-e94e-456c-870f-c18178498e78, rgb(255, 215, 0))"
+                  width="18px"
+                  height="18px"
+                >
+                  <path d="M12 17.27L18.18 21l-1.64-7.03L22 9.24l-7.19-.61L12 2 9.19 8.63 2 9.24l5.46 4.73L5.82 21z"></path>
+                </svg>
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  viewBox="0 0 24 24"
+                  fill="var(--token-c858731e-e94e-456c-870f-c18178498e78, rgb(255, 215, 0))"
+                  width="18px"
+                  height="18px"
+                >
+                  <path d="M12 17.27L18.18 21l-1.64-7.03L22 9.24l-7.19-.61L12 2 9.19 8.63 2 9.24l5.46 4.73L5.82 21z"></path>
+                </svg>
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  viewBox="0 0 24 24"
+                  fill="var(--token-c858731e-e94e-456c-870f-c18178498e78, rgb(255, 215, 0))"
+                  width="18px"
+                  height="18px"
+                >
+                  <path d="M12 17.27L18.18 21l-1.64-7.03L22 9.24l-7.19-.61L12 2 9.19 8.63 2 9.24l5.46 4.73L5.82 21z"></path>
+                </svg>
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  viewBox="0 0 24 24"
+                  fill="var(--token-c858731e-e94e-456c-870f-c18178498e78, rgb(255, 215, 0))"
+                  width="18px"
+                  height="18px"
+                >
+                  <path d="M12 17.27L18.18 21l-1.64-7.03L22 9.24l-7.19-.61L12 2 9.19 8.63 2 9.24l5.46 4.73L5.82 21z"></path>
+                </svg>
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  viewBox="0 0 24 24"
+                  fill="var(--token-c858731e-e94e-456c-870f-c18178498e78, rgb(255, 215, 0))"
+                  width="18px"
+                  height="18px"
+                >
+                  <path d="M12 17.27L18.18 21l-1.64-7.03L22 9.24l-7.19-.61L12 2 9.19 8.63 2 9.24l5.46 4.73L5.82 21z"></path>
+                </svg>
+              </div>
+            </div>
+          </div>
+
+          <div className="vertical-slide-content">
+            <p>"I was able to adapt the templates to fit my unique needs perfectly. Couldn't be happier!"</p>
+
+          </div>
+        </div>
+        <div className="vertical-slide">
+          <div className="vertical-slide-heading-container">
+            <div className="vertical-slide-img">
+              <img
+                src="https://framerusercontent.com/images/lN1CfuAulx1IJ6VQhc4Pbe1sXSA.png"
+                alt=""
+              />
+            </div>
+
+            <div className="slider-heading">
+              <h5>Jack</h5>
+              <div class="Testimonial-rating">
+                <p class="m-0">5</p>
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  viewBox="0 0 24 24"
+                  fill="var(--token-c858731e-e94e-456c-870f-c18178498e78, rgb(255, 215, 0))"
+                  width="18px"
+                  height="18px"
+                >
+                  <path d="M12 17.27L18.18 21l-1.64-7.03L22 9.24l-7.19-.61L12 2 9.19 8.63 2 9.24l5.46 4.73L5.82 21z"></path>
+                </svg>
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  viewBox="0 0 24 24"
+                  fill="var(--token-c858731e-e94e-456c-870f-c18178498e78, rgb(255, 215, 0))"
+                  width="18px"
+                  height="18px"
+                >
+                  <path d="M12 17.27L18.18 21l-1.64-7.03L22 9.24l-7.19-.61L12 2 9.19 8.63 2 9.24l5.46 4.73L5.82 21z"></path>
+                </svg>
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  viewBox="0 0 24 24"
+                  fill="var(--token-c858731e-e94e-456c-870f-c18178498e78, rgb(255, 215, 0))"
+                  width="18px"
+                  height="18px"
+                >
+                  <path d="M12 17.27L18.18 21l-1.64-7.03L22 9.24l-7.19-.61L12 2 9.19 8.63 2 9.24l5.46 4.73L5.82 21z"></path>
+                </svg>
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  viewBox="0 0 24 24"
+                  fill="var(--token-c858731e-e94e-456c-870f-c18178498e78, rgb(255, 215, 0))"
+                  width="18px"
+                  height="18px"
+                >
+                  <path d="M12 17.27L18.18 21l-1.64-7.03L22 9.24l-7.19-.61L12 2 9.19 8.63 2 9.24l5.46 4.73L5.82 21z"></path>
+                </svg>
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  viewBox="0 0 24 24"
+                  fill="var(--token-c858731e-e94e-456c-870f-c18178498e78, rgb(255, 215, 0))"
+                  width="18px"
+                  height="18px"
+                >
+                  <path d="M12 17.27L18.18 21l-1.64-7.03L22 9.24l-7.19-.61L12 2 9.19 8.63 2 9.24l5.46 4.73L5.82 21z"></path>
+                </svg>
+              </div>
+            </div>
+          </div>
+
+          <div className="vertical-slide-content">
+            <p>"I was able to adapt the templates to fit my unique needs perfectly. Couldn't be happier!"</p>
+
+          </div>
+        </div>
       </div>
       <div className="vertical-slider-down">
         <div className="vertical-slide">
@@ -275,8 +409,276 @@ const TestimonialSlider = () => {
 
           </div>
         </div>
+        <div className="vertical-slide">
+          <div className="vertical-slide-heading-container">
+            <div className="vertical-slide-img">
+              <img
+                src="https://framerusercontent.com/images/lN1CfuAulx1IJ6VQhc4Pbe1sXSA.png"
+                alt=""
+              />
+            </div>
+
+            <div className="slider-heading">
+              <h5>Jack</h5>
+              <div class="Testimonial-rating">
+                <p class="m-0">5</p>
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  viewBox="0 0 24 24"
+                  fill="var(--token-c858731e-e94e-456c-870f-c18178498e78, rgb(255, 215, 0))"
+                  width="18px"
+                  height="18px"
+                >
+                  <path d="M12 17.27L18.18 21l-1.64-7.03L22 9.24l-7.19-.61L12 2 9.19 8.63 2 9.24l5.46 4.73L5.82 21z"></path>
+                </svg>
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  viewBox="0 0 24 24"
+                  fill="var(--token-c858731e-e94e-456c-870f-c18178498e78, rgb(255, 215, 0))"
+                  width="18px"
+                  height="18px"
+                >
+                  <path d="M12 17.27L18.18 21l-1.64-7.03L22 9.24l-7.19-.61L12 2 9.19 8.63 2 9.24l5.46 4.73L5.82 21z"></path>
+                </svg>
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  viewBox="0 0 24 24"
+                  fill="var(--token-c858731e-e94e-456c-870f-c18178498e78, rgb(255, 215, 0))"
+                  width="18px"
+                  height="18px"
+                >
+                  <path d="M12 17.27L18.18 21l-1.64-7.03L22 9.24l-7.19-.61L12 2 9.19 8.63 2 9.24l5.46 4.73L5.82 21z"></path>
+                </svg>
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  viewBox="0 0 24 24"
+                  fill="var(--token-c858731e-e94e-456c-870f-c18178498e78, rgb(255, 215, 0))"
+                  width="18px"
+                  height="18px"
+                >
+                  <path d="M12 17.27L18.18 21l-1.64-7.03L22 9.24l-7.19-.61L12 2 9.19 8.63 2 9.24l5.46 4.73L5.82 21z"></path>
+                </svg>
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  viewBox="0 0 24 24"
+                  fill="var(--token-c858731e-e94e-456c-870f-c18178498e78, rgb(255, 215, 0))"
+                  width="18px"
+                  height="18px"
+                >
+                  <path d="M12 17.27L18.18 21l-1.64-7.03L22 9.24l-7.19-.61L12 2 9.19 8.63 2 9.24l5.46 4.73L5.82 21z"></path>
+                </svg>
+              </div>
+            </div>
+          </div>
+
+          <div className="vertical-slide-content">
+            <p>"I was able to adapt the templates to fit my unique needs perfectly. Couldn't be happier!"</p>
+
+          </div>
+        </div>
+        <div className="vertical-slide">
+          <div className="vertical-slide-heading-container">
+            <div className="vertical-slide-img">
+              <img
+                src="https://framerusercontent.com/images/lN1CfuAulx1IJ6VQhc4Pbe1sXSA.png"
+                alt=""
+              />
+            </div>
+
+            <div className="slider-heading">
+              <h5>Jack</h5>
+              <div class="Testimonial-rating">
+                <p class="m-0">5</p>
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  viewBox="0 0 24 24"
+                  fill="var(--token-c858731e-e94e-456c-870f-c18178498e78, rgb(255, 215, 0))"
+                  width="18px"
+                  height="18px"
+                >
+                  <path d="M12 17.27L18.18 21l-1.64-7.03L22 9.24l-7.19-.61L12 2 9.19 8.63 2 9.24l5.46 4.73L5.82 21z"></path>
+                </svg>
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  viewBox="0 0 24 24"
+                  fill="var(--token-c858731e-e94e-456c-870f-c18178498e78, rgb(255, 215, 0))"
+                  width="18px"
+                  height="18px"
+                >
+                  <path d="M12 17.27L18.18 21l-1.64-7.03L22 9.24l-7.19-.61L12 2 9.19 8.63 2 9.24l5.46 4.73L5.82 21z"></path>
+                </svg>
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  viewBox="0 0 24 24"
+                  fill="var(--token-c858731e-e94e-456c-870f-c18178498e78, rgb(255, 215, 0))"
+                  width="18px"
+                  height="18px"
+                >
+                  <path d="M12 17.27L18.18 21l-1.64-7.03L22 9.24l-7.19-.61L12 2 9.19 8.63 2 9.24l5.46 4.73L5.82 21z"></path>
+                </svg>
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  viewBox="0 0 24 24"
+                  fill="var(--token-c858731e-e94e-456c-870f-c18178498e78, rgb(255, 215, 0))"
+                  width="18px"
+                  height="18px"
+                >
+                  <path d="M12 17.27L18.18 21l-1.64-7.03L22 9.24l-7.19-.61L12 2 9.19 8.63 2 9.24l5.46 4.73L5.82 21z"></path>
+                </svg>
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  viewBox="0 0 24 24"
+                  fill="var(--token-c858731e-e94e-456c-870f-c18178498e78, rgb(255, 215, 0))"
+                  width="18px"
+                  height="18px"
+                >
+                  <path d="M12 17.27L18.18 21l-1.64-7.03L22 9.24l-7.19-.61L12 2 9.19 8.63 2 9.24l5.46 4.73L5.82 21z"></path>
+                </svg>
+              </div>
+            </div>
+          </div>
+
+          <div className="vertical-slide-content">
+            <p>"I was able to adapt the templates to fit my unique needs perfectly. Couldn't be happier!"</p>
+
+          </div>
+        </div>
       </div>
       <div className="vertical-slider-up">
+        <div className="vertical-slide">
+          <div className="vertical-slide-heading-container">
+            <div className="vertical-slide-img">
+              <img
+                src="https://framerusercontent.com/images/lN1CfuAulx1IJ6VQhc4Pbe1sXSA.png"
+                alt=""
+              />
+            </div>
+
+            <div className="slider-heading">
+              <h5>Jack</h5>
+              <div class="Testimonial-rating">
+                <p class="m-0">5</p>
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  viewBox="0 0 24 24"
+                  fill="var(--token-c858731e-e94e-456c-870f-c18178498e78, rgb(255, 215, 0))"
+                  width="18px"
+                  height="18px"
+                >
+                  <path d="M12 17.27L18.18 21l-1.64-7.03L22 9.24l-7.19-.61L12 2 9.19 8.63 2 9.24l5.46 4.73L5.82 21z"></path>
+                </svg>
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  viewBox="0 0 24 24"
+                  fill="var(--token-c858731e-e94e-456c-870f-c18178498e78, rgb(255, 215, 0))"
+                  width="18px"
+                  height="18px"
+                >
+                  <path d="M12 17.27L18.18 21l-1.64-7.03L22 9.24l-7.19-.61L12 2 9.19 8.63 2 9.24l5.46 4.73L5.82 21z"></path>
+                </svg>
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  viewBox="0 0 24 24"
+                  fill="var(--token-c858731e-e94e-456c-870f-c18178498e78, rgb(255, 215, 0))"
+                  width="18px"
+                  height="18px"
+                >
+                  <path d="M12 17.27L18.18 21l-1.64-7.03L22 9.24l-7.19-.61L12 2 9.19 8.63 2 9.24l5.46 4.73L5.82 21z"></path>
+                </svg>
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  viewBox="0 0 24 24"
+                  fill="var(--token-c858731e-e94e-456c-870f-c18178498e78, rgb(255, 215, 0))"
+                  width="18px"
+                  height="18px"
+                >
+                  <path d="M12 17.27L18.18 21l-1.64-7.03L22 9.24l-7.19-.61L12 2 9.19 8.63 2 9.24l5.46 4.73L5.82 21z"></path>
+                </svg>
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  viewBox="0 0 24 24"
+                  fill="var(--token-c858731e-e94e-456c-870f-c18178498e78, rgb(255, 215, 0))"
+                  width="18px"
+                  height="18px"
+                >
+                  <path d="M12 17.27L18.18 21l-1.64-7.03L22 9.24l-7.19-.61L12 2 9.19 8.63 2 9.24l5.46 4.73L5.82 21z"></path>
+                </svg>
+              </div>
+            </div>
+          </div>
+
+          <div className="vertical-slide-content">
+            <p>"I was able to adapt the templates to fit my unique needs perfectly. Couldn't be happier!"</p>
+
+          </div>
+        </div>
+        <div className="vertical-slide">
+          <div className="vertical-slide-heading-container">
+            <div className="vertical-slide-img">
+              <img
+                src="https://framerusercontent.com/images/lN1CfuAulx1IJ6VQhc4Pbe1sXSA.png"
+                alt=""
+              />
+            </div>
+
+            <div className="slider-heading">
+              <h5>Jack</h5>
+              <div class="Testimonial-rating">
+                <p class="m-0">5</p>
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  viewBox="0 0 24 24"
+                  fill="var(--token-c858731e-e94e-456c-870f-c18178498e78, rgb(255, 215, 0))"
+                  width="18px"
+                  height="18px"
+                >
+                  <path d="M12 17.27L18.18 21l-1.64-7.03L22 9.24l-7.19-.61L12 2 9.19 8.63 2 9.24l5.46 4.73L5.82 21z"></path>
+                </svg>
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  viewBox="0 0 24 24"
+                  fill="var(--token-c858731e-e94e-456c-870f-c18178498e78, rgb(255, 215, 0))"
+                  width="18px"
+                  height="18px"
+                >
+                  <path d="M12 17.27L18.18 21l-1.64-7.03L22 9.24l-7.19-.61L12 2 9.19 8.63 2 9.24l5.46 4.73L5.82 21z"></path>
+                </svg>
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  viewBox="0 0 24 24"
+                  fill="var(--token-c858731e-e94e-456c-870f-c18178498e78, rgb(255, 215, 0))"
+                  width="18px"
+                  height="18px"
+                >
+                  <path d="M12 17.27L18.18 21l-1.64-7.03L22 9.24l-7.19-.61L12 2 9.19 8.63 2 9.24l5.46 4.73L5.82 21z"></path>
+                </svg>
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  viewBox="0 0 24 24"
+                  fill="var(--token-c858731e-e94e-456c-870f-c18178498e78, rgb(255, 215, 0))"
+                  width="18px"
+                  height="18px"
+                >
+                  <path d="M12 17.27L18.18 21l-1.64-7.03L22 9.24l-7.19-.61L12 2 9.19 8.63 2 9.24l5.46 4.73L5.82 21z"></path>
+                </svg>
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  viewBox="0 0 24 24"
+                  fill="var(--token-c858731e-e94e-456c-870f-c18178498e78, rgb(255, 215, 0))"
+                  width="18px"
+                  height="18px"
+                >
+                  <path d="M12 17.27L18.18 21l-1.64-7.03L22 9.24l-7.19-.61L12 2 9.19 8.63 2 9.24l5.46 4.73L5.82 21z"></path>
+                </svg>
+              </div>
+            </div>
+          </div>
+
+          <div className="vertical-slide-content">
+            <p>"I was able to adapt the templates to fit my unique needs perfectly. Couldn't be happier!"</p>
+
+          </div>
+        </div>
         <div className="vertical-slide">
           <div className="vertical-slide-heading-container">
             <div className="vertical-slide-img">
