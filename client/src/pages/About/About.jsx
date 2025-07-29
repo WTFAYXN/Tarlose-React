d96617fd30886554c2194f48e594874574c4d2be
jahangir2002 <jahangirshaikh553@gmail.com>
2025-07-29 16:17:22 +0530

    feat: Add About page content and styling; implement hero section with text and experience details for enhanced user engagement

--- client/src/pages/About/About.jsx
+++ client/src/pages/About/About.jsx
@@ -1,31 +1,42 @@
-import React from 'react'
-import Navbar from '../../components/Navbar'
-import Footer from '../../components/Footer'
-import Cursor from '../../components/Cursor'
+import React from "react";
+import Navbar from "../../components/Navbar";
+import Footer from "../../components/Footer";
+import Cursor from "../../components/Cursor";
+
+import "./about.css";
 
 const About = () => {
   return (
     <>
-    <Navbar />
+      <Navbar />
 
-    <Cursor />
+      <Cursor />
 
-    <div className='about-hero'>
+      <div className="about-hero container">
+        <p>We get it done.</p>
+        <h1>CONCEPTS <span className="eco-sub">DESIGNED</span>  TO MAKE AN IMPACT</h1>
+      </div>
 
-        <h1></h1>
+      <div className="about-tarlose container">
+        <h5>ABOUT Tarlose</h5>
 
-    </div>
+        <div className="about-experience-container">
+            <h2>not old, just experienced.</h2>
+            <p>We've been around since 2009, and we've seen the industry change and grow.</p>
+            <p>We have collaborated on an impressive list of high-profile interactive projects through partnerships with some of the top advertising agencies and brands in the world. Our work has ranged from entertainment and consumer goods, to manufacturing, technology, and more.</p>
+            <button type="button" className="nav-btn">
+            <a href="https://calendly.com/ayansayad2005/30min?month=2024-12">
+              Book a Call
+            </a>
+          </button>
 
+        </div>
 
+      </div>
 
-
-
-
-
-
-    <Footer />
+      <Footer />
     </>
-  )
-}
+  );
+};
 
-export default About
+export default About;
