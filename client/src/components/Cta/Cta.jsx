f522e36fe2aeb72d16b491bc8fa00877a567c9d8
jahangir2002 <jahangirshaikh553@gmail.com>
2025-08-02 12:29:55 +0530

    style: Update Cta component CSS for improved aesthetics and mobile responsiveness; adjust background, margins, and border-radius, and enhance layout in JSX

--- client/src/components/Cta/Cta.jsx
+++ client/src/components/Cta/Cta.jsx
@@ -4,14 +4,16 @@ import "./Cta.css";
 const Cta = () => {
   return (
     <>
-      <div className="section-cta container">
-        <div className="cta-container">
-          <h2>Start your project with Alture <span className="cta-span">®</span></h2>
-            <button type="button" className="nav-btn">
-              <a href="https://calendly.com/ayansayad2005/30min?month=2024-12">
-                Book a Call
-              </a>
-            </button>
+      <div className="section-cta">
+        <div className="cta-container container">
+          <h2>
+            Start your project with Alture <span className="cta-span">®</span>
+          </h2>
+          <button type="button" className="nav-btn">
+            <a href="https://calendly.com/ayansayad2005/30min?month=2024-12">
+              Book a Call
+            </a>
+          </button>
         </div>
       </div>
     </>
